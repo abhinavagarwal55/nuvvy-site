4d7220a80e4a5399df28aced21443a4dff3dadca
abhinavagarwal55 <abhinav.agarwal55@gmail.com>
2025-11-02 15:54:24 +0530

    chore: disable react/no-unescaped-entities and ignore ESLint during builds

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  eslint: { ignoreDuringBuilds: true },
   // Allow remote images used in the Inspiration Gallery
   images: {
     remotePatterns: [
