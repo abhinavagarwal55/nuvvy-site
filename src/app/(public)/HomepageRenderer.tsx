6e4fe22a8b21115f378f615a8fbef4165b9de303
abhinavagarwal55 <abhinav.agarwal55@gmail.com>
2026-02-15 20:43:01 +0530

    fix1

--- src/app/(public)/HomepageRenderer.tsx
+++ src/app/(public)/HomepageRenderer.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import { Leaf, Droplet, IndianRupee } from "lucide-react";
+import { X, Leaf, Droplet, IndianRupee } from "lucide-react";
 import Link from "next/link";
 import Image from "next/image";
 import SnabbitHero from "@/components/heroes/SnabbitHero";
